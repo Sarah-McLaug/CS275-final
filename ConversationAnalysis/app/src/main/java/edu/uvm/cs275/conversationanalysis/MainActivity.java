3299c0421a4f9575f62f80f7f981afeb70005c32
Christopher Thomas Suitor <christopher.suitor@uvm.edu>
2019-10-06 14:18:01 -0400

    added the navigation menu (it doesn't do nagivation... yet). Also, did some styling.

--- ConversationAnalysis/app/src/main/java/edu/uvm/cs275/conversationanalysis/MainActivity.java
+++ ConversationAnalysis/app/src/main/java/edu/uvm/cs275/conversationanalysis/MainActivity.java
@@ -1,25 +1,44 @@
 package edu.uvm.cs275.conversationanalysis;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mNavDrawer;
     private ImageButton mRecordButton;
+    private Button mMenuButton;
     private TextView mContactInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNavDrawer = (DrawerLayout) findViewById(R.id.drawer_layout); // grab the navigation drawer
 
         buttonPress();
+    }
+
+    /* Override the back button if the navigation drawer is open. If it is open, we want the back
+    *  button to close the menu, not the entire activity. */
+    @Override
+    public void onBackPressed(){
+        if(mNavDrawer.isDrawerOpen(GravityCompat.END)){
+            mNavDrawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     // This method contains the calls for when a button is pressed.
@@ -40,6 +59,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 mContactInfo.setText(R.string.contact_email);
+            }
+        });
+
+        // Pressing the menu button
+        mMenuButton = (Button) findViewById(R.id.menu_button);
+        mMenuButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(!mNavDrawer.isDrawerOpen(Gravity.RIGHT)){
+                    mNavDrawer.openDrawer(Gravity.RIGHT);
+                } else {
+                    mNavDrawer.closeDrawer(Gravity.LEFT);
+                }
             }
         });
     }
