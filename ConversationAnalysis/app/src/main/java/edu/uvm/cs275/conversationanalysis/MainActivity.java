5c60c5c9759194c53b18e3850036d7805bf7435b
sarahmclaughlin <smartymcsmartle@gmail.com>
2019-10-08 11:14:51 -0400

    started adding the microphone functionality, not implemented yet

--- ConversationAnalysis/app/src/main/java/edu/uvm/cs275/conversationanalysis/MainActivity.java
+++ ConversationAnalysis/app/src/main/java/edu/uvm/cs275/conversationanalysis/MainActivity.java
@@ -1,11 +1,16 @@
 package edu.uvm.cs275.conversationanalysis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +19,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "Audio Recording";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String fileName = null;
 
     private DrawerLayout mNavDrawer;
     private ImageButton mRecordButton;
     private Button mMenuButton;
     private TextView mContactInfo;
+    private MediaRecorder mRecorder;
+
+    private boolean recordPermission = false;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,37 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    /* Override the permissions request result
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                recordPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if(!recordPermission) finish();
+    }
+
+    // This method starts recording audio
+    private void startRecording(){
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mRecorder.setOutputFile(fileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e){
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
     }
 
     // This method contains the calls for when a button is pressed.
