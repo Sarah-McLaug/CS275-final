8b5cd89362fbe4da42141a371c6c6bac14662035
Devon Havers <Devon66h@gmail.com>
2019-10-31 10:23:15 -0400

    changed audio recording type

--- ConversationAnalysis/app/src/main/java/edu/uvm/cs275/conversationanalysis/MainActivity.java
+++ ConversationAnalysis/app/src/main/java/edu/uvm/cs275/conversationanalysis/MainActivity.java
@@ -26,9 +26,6 @@ import com.google.android.material.navigation.NavigationView;
 import java.io.IOException;
 import java.util.UUID;
 
-import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
-import cafe.adriel.androidaudioconverter.callback.ILoadCallback;
-
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String LOG_TAG = "Audio Recording";
@@ -56,19 +53,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // initialize audio converter
-        AndroidAudioConverter.load(this, new ILoadCallback() {
-            @Override
-            public void onSuccess() {
-                Log.i(LOG_TAG, "AudioConverter success!");
-            }
-            @Override
-            public void onFailure(Exception error) {
-                Log.i(LOG_TAG, "FFmpeg is not supported by device");
-            }
-        });
-
         mConversationManager = ConversationManager.getInstance(this);
         setContentView(R.layout.activity_main);
         mNavDrawer = findViewById(R.id.drawer_layout); // grab the navigation drawer
@@ -149,7 +133,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mStopButton.setVisibility(View.VISIBLE);
 
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);
         mRecorder.setOutputFile(ProcessingActivity.getAudioFile(getApplicationContext()));
         mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
